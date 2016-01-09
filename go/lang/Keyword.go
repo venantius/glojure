d33aaa02f3c921cc7c09e050d1a2b5061fdf6e07
David Jarvis <venantius@gmail.com>
2016-01-09 01:03:27 -0800

    Starting to flesh out the LispReader

--- go/lang/Keyword.go
+++ go/lang/Keyword.go
@@ -1,20 +1,53 @@
 package lang
 
+/*
+	Keywords involve more concurrent magic than I had previously anticipated;
+	the most noticable of which are the combination use of a ReferenceQueue and
+	a ConcurrentHashMap where the values are Reference<Keyword>. None of these
+	structures have direct analogs in Go -- for the time being, I've punted
+	on the issue firmly, but it'll have to be revisited at some point.
+
+	~ @venantius, jan 2016
+*/
+
 // NOTE: Implements IFn, Comparable, Named, Serializable, IHashEq
 type Keyword struct {
-	table  int // TODO: Should be ConcurrentHashMap ???
-	rq     int // should be ReferenceQueue
-	sym    Symbol
+	sym    *Symbol
 	hasheq int
-	_str   string
+	_str   *string
 }
 
-/*
-	TODO: This method is overloaded in Java
-	takes (symbol), (str, str), (str)
-*/
-func (k *Keyword) Intern(sym Symbol) *Keyword {
+var rq int                                   // Should be ReferenceQueue
+var keywordTable = make(map[Symbol]*Keyword) // Should be ConcurrentHashMap with Reference<Keyword>
+
+// TODO
+func InternKeyword(sym *Symbol) *Keyword {
+	var k *Keyword
+	existingRef := keywordTable[*sym]
+	if existingRef == nil {
+		// TODO: Util.ClearCache(rq, table)
+		if sym.Meta() != nil {
+			sym = sym.WithMeta(nil).(*Symbol)
+		}
+		k = &Keyword{
+			sym:    sym,
+			hasheq: sym.HashEq() + 0x9e3779b9,
+		}
+		// TODO
+	}
+	if existingRef == nil {
+		return k
+	}
+	//TODO...more
 	return nil
+}
+
+func InternKeywordNsAndName(ns *string, name *string) *Keyword {
+	return InternKeyword(InternSymbolNsAndName(*ns, *name))
+}
+
+func InternKeywordNsName(nsname string) *Keyword {
+	return InternKeyword(InternSymbol(nsname))
 }
 
 /*
@@ -25,11 +58,52 @@ func (k *Keyword) Find(sym Symbol) *Keyword {
 	return nil
 }
 
-// TODO
 func (k *Keyword) HashCode() int {
-	return 0
+	return k.sym.HashCode() + 0x9e3779b9
 }
 
 func (k *Keyword) HashEq() int {
 	return k.hasheq
 }
+
+func (k *Keyword) String() string {
+	if k._str == nil {
+		*k._str = ":" + k.sym.String()
+	}
+	return *k._str
+}
+
+// TODO: A bit longer actually
+func (k *Keyword) ThrowArity() interface{} {
+	panic(IllegalArgumentException)
+}
+
+func (k *Keyword) Call() interface{} {
+	return k.ThrowArity()
+}
+
+func (k *Keyword) Run() {
+	panic(UnsupportedOperationException)
+}
+
+func (k *Keyword) Invoke() interface{} {
+	return k.ThrowArity()
+}
+
+func (k *Keyword) CompareTo(o interface{}) int {
+	return k.sym.CompareTo(o.(Keyword).sym)
+}
+
+func (k *Keyword) GetNamespace() string {
+	return k.sym.GetNamespace()
+}
+
+func (k *Keyword) GetName() string {
+	return k.sym.GetName()
+}
+
+func (k *Keyword) ReadResolve() interface{} {
+	return InternKeyword(k.sym)
+}
+
+// TODO... the rest of this file?
